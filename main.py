992a031a1b41688fc751260d7ec55d2cd0fb9f3d
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-04-30 02:19:02 +0600

    Adding main

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-from turtle import Turtle
+from turtle import Screen
+
+screen = Screen()
+screen.bgcolor("white")
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+
+
+screen.exitonclick()
