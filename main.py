2862a834efc9d5b6d6099306d9ddfd6207bcf57a
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-04-30 02:21:58 +0600

    Adding main

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from turtle import Screen
+import time
 
 screen = Screen()
 screen.bgcolor("white")
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+game_is_on = True
+
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
 
 
 screen.exitonclick()
