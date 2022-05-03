ef371f1c8abf1667be35d2d48002341907e7f2b3
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-03 20:18:18 +0600

    Adding multiple players.

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     transports1 = Transport()
-    transports2 = Transport()
     transports1.auto_move()
+    transports2 = Transport()
     transports2.auto_move()
     screen.update()
 
