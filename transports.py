5b921cf16a8f233791171d024f55d946ef26a186
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-04 06:52:52 +0600

    Moving transports.

--- transports.py
+++ transports.py
@@ -11,13 +11,15 @@ class Transport(Turtle):
         self.all_cars = []
 
     def create_car(self):
-        new_car = Turtle("square")
-        new_car.shapesize(stretch_wid=1, stretch_len=2)
-        new_car.penup()
-        new_car.color(random.choice(COLORS))
-        random_y = random.randint(-250, 250)
-        new_car.goto(320, random_y)
-        self.all_cars.append(new_car)
+        random_choice = random.randint(1, 6)    #Speed Control
+        if random_choice == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            new_car.goto(320, random_y)
+            self.all_cars.append(new_car)
 
     def auto_move(self):
         for car in self.all_cars:
