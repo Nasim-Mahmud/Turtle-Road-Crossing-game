0ae0dd4591f554936989a35e77aaa1c82ba33ec7
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-05 09:48:28 +0600

    Resetting player position after certain y_cor.

--- scoreboard.py
+++ scoreboard.py
@@ -1,13 +1,16 @@
 from turtle import Turtle
 
 FONT = ("Courier", 16, "bold")
-
+LEVEL = 1
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.level = 1
+        self.level = LEVEL
         self.ht()
         self.penup()
-        self.goto(-280, 270)
+        self.goto(-280, 260)
         self.write(f"Level: {self.level}", align="Left", font=FONT)
+
+    def upgrade_level(self):
+        self.level += 1
